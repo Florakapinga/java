72e3f8c28da65f2263bd4b6e5bfc545b7203ebf2
kapinga <erickfloriana@gmail.com>
2025-04-29 11:27:58 +0300

    while loop

---
+++ loops.java
@@ -0,0 +1,8 @@
+public class loops {
+    public static void main(String[] args) {
+        int x=10;
+        while (x<10) {
+System.out.println("value of x:" +x);x++;            
+        }
+    }
+}
